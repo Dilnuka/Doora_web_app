c5daa1554b4b821a55270c18def73351238df28c
Dilnuka Seniru Liyanage <sendliyanage@gmail.com>
2026-06-02 16:10:05 +0530

    feat: enhance authentication configuration and improve Prisma client initialization

--- auth.config.js
+++ auth.config.js
@@ -1,4 +1,6 @@
 export const authConfig = {
+  trustHost: true,
+  secret: process.env.AUTH_SECRET ?? process.env.NEXTAUTH_SECRET,
   session: { strategy: "jwt" },
   providers: [],
   pages: {
--- auth.js
+++ auth.js
@@ -1,13 +1,11 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
-import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "./lib/prisma"
 import bcrypt from "bcryptjs"
 import { authConfig } from "./auth.config"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
       name: "Credentials",
